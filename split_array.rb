814f743c9a9174fa135d1ff8ebe035ebae35aa25
[Goutam] <[goutam.singireddy@gmail.com]>
2015-08-14 14:46:13 -0400

    adding split prog

---
+++ split_array.rb
@@ -0,0 +1,29 @@
+
+
+
+
+# a=["a","b","c","w","1","2","3","2","3","4","4","3" ]
+   # word= gets.chomp
+   # a = word.split(/\.?\s+/)
+  # sort=a.sort_by do |a|
+  
+ # end
+   
+   # print "abcw12323443" 
+   # word=gets.chomp
+   # a=word.split(/\.?\s+/)
+   # puts "#{a.first}"
+   
+   # a = "abcw12323443"  
+   # a.split('').each do|c| 
+   # puts c end
+   
+   
+   
+   
+  # a="abcw12323443".split(abc=$;,[0])
+   
+ 
+  
+ 
+  
